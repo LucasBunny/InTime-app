49337a723b85452d4986d458757f2d0a329cb8b8
Lucas_Bunny <lucas08ms@gmail.com>
2022-12-04 21:49:14 -0300

    Ajustes de Integração com o Banco de Dados - Em andamento...

--- App/app.py
+++ App/app.py
@@ -1,20 +1,22 @@
 import pyodbc
+import locale
+import webbrowser
 import pandas as pd
 from itertools import cycle
 from kivy.clock import Clock
 from kivy.lang import Builder
+from datetime import datetime
 from kivymd.uix.card import MDCard
 from kivy.core.window import Window
 from kivy.animation import Animation
 from kivymd.uix.dialog import MDDialog
 from kivy.uix.screenmanager import Screen
-from kivy.uix.scrollview import ScrollView
-from kivymd.uix.button import MDFlatButton
+from kivymd.uix.textfield import MDTextField
 from kivymd.uix.button import MDRaisedButton
 from kivymd.tools.hotreload.app import MDApp
-from kivymd.uix.behaviors import HoverBehavior
 from kivymd.uix.anchorlayout import AnchorLayout
 from kivymd.uix.floatlayout import MDFloatLayout
+from kivymd.uix.button import MDFlatButton, MDIconButton
 from kivy.properties import ListProperty, NumericProperty, StringProperty, BooleanProperty
 from kivymd.uix.behaviors.backgroundcolor_behavior import BackgroundColorBehavior
 
@@ -147,6 +149,22 @@ class Login(Screen, BackgroundColorBehavior):
             SELECT * FROM Usuario WHERE User_email='{email}' AND User_senha='{senha}'
             """
         )
+        
+        tab_usuario = cursor.execute(
+            f"""
+            SELECT * FROM Usuario WHERE User_email='{email}' AND User_senha='{senha}'
+            """
+        )
+        tab_tarefas = cursor.execute(
+            f"""
+            SELECT * FROM Tarefas WHERE Id_user=...
+            """
+        )
+        tab_lembretes = cursor.execute(
+            f"""
+            SELECT * FROM Lembretes WHERE Id_user=...
+            """
+        )
         for resutl in query:
             resutl = list(resutl)
             banco.append(resutl)
@@ -154,13 +172,17 @@ class Login(Screen, BackgroundColorBehavior):
 
         if user.empty:
             self.show_alert()
-
         else:
             Clock.schedule_once(self.change_screen, 1)
+            with open('App/log.txt', 'w') as f:
+                f.write(user.to_string(index=False))
            
 class Cadastrar(Screen, BackgroundColorBehavior):
     genero = 'Null'
     termos = False
+
+    def terms(self):
+        webbrowser.open('https://www.youtube.com')
 
     def close_alert(self, obj):
         self.alert.dismiss()
@@ -214,7 +236,9 @@ class Cadastrar(Screen, BackgroundColorBehavior):
              self.show_alert()
 
 class Tela_Relatorios(Screen, BackgroundColorBehavior):
-    pass
+    inf_data = StringProperty('')
+    locale.setlocale(locale.LC_TIME, 'pt_BR')
+    info_data = datetime.today().strftime('%A, %d de %B de %Y')
 
 class Tela_Configuracoes(Screen, BackgroundColorBehavior):
     pass
@@ -226,44 +250,217 @@ class Tela_Configuracoes_About(Screen, BackgroundColorBehavior):
     pass
 
 class Tela_Tarefas(Screen, BackgroundColorBehavior):
-    class Tarefa(MDCard, HoverBehavior):
-        titulo = StringProperty('Este aqui é um Título')
-        data = StringProperty('20/11')
-        text = StringProperty('Exemplo aqui esta, para mostrar que deu tudo certo!!! Vamos continuar escrevendo algo só para ver ser ele aguenta colocar muito texto :3')
-        click = 0
+    class Tarefa(MDCard):
+    
+        titulo = StringProperty('')
+        data = StringProperty('')
+        text = StringProperty('')
+        click = 1
 
-        def user_click(self):
-            if self.click == 0:
+        def close_alert(self, obj):
+            self.alert.dismiss()
+        def show_alert(self):
+            close_btn = MDFlatButton(text='Ok', on_release=self.close_alert)
+            self.alert = MDDialog( title="Aviso", text="Valores incorretos. Por favor insira os valores de maneira a respeitar os campos.", buttons=[close_btn])
+            self.alert.open()
+
+        def user_click_open(self):
+            t = Tela_Tarefas()
+            if self.click == 1:
                 Animation(
                     size=(300, 300), 
                     duration=0.1,
                     t='in_quad',
                 ).start(self)
-                self.ids.label_titulo.pos=(3, 100)
-                self.ids.label_data.pos=(240, 100)
-                self.ids.label_texto.pos=(3, 40)
 
-                self.ids.layout_tarf.add_widget(MDRaisedButton(text='Cancelar', pos_hint={'center_x':.1, 'center_y':.5}))
-                self.ids.layout_tarf.add_widget(MDRaisedButton(text='Salvar', pos_hint={'center_x':.5, 'center_y':.5}))
-                self.ids.layout_tarf.add_widget(MDRaisedButton(text='Finalizar Tarefa', pos_hint={'center_x':.9, 'center_y':.5}))
-                self.click = 1
-            else:
+                self.btn1 = MDRaisedButton(text='Excluir', pos_hint={'center_x':.1, 'center_y':.07}, size_hint=(.2, 0.06), on_release=lambda x:t.excluir()) 
+                self.btn2 = MDRaisedButton(text='Salvar', pos_hint={'center_x':.47, 'center_y':.07}, size_hint=(.3, 0.06), on_release=lambda x:self.salvar()) 
+                self.btn3 = MDRaisedButton(text='Finalizar Tarefa', pos_hint={'center_x':.9, 'center_y':.07}, size_hint=(.2, 0.06)) 
+                self.btn4 = MDIconButton(icon='close', pos_hint={'center_x':.05, 'center_y':.96}, on_release=lambda x:self.user_click_close())
+
+                self.label_title = MDTextField(text=self.titulo, hint_text='Título', size_hint=(.5, None), pos_hint={'center_x': .3, 'center_y': 0.85}, multiline=True, line_color_normal='white', line_color_focus="white", text_color_normal='black', text_color_focus='black', hint_text_color_focus="black",  max_text_length=15)
+                self.label_text = MDTextField(text=self.text, hint_text='Breve descrição da Tarefa.', size_hint=(.9, None), pos_hint={'center_x': .51, 'center_y': .649}, multiline=True, line_color_normal='white', line_color_focus="white", text_color_normal='black', text_color_focus='black', hint_text_color_focus="black", max_text_length=84)
+                self.label_data = MDTextField(text=self.data, hint_text='Conclusão', helper_text='yyyy/mm/dd', size_hint=(.3, None), pos_hint={'center_x': 0.83, 'center_y': 0.85}, validator="date", date_format="yyyy/mm/dd", line_color_normal='white', line_color_focus="white", text_color_normal='black', text_color_focus='black', hint_text_color_focus="black")
+                
+                self.titulo = ''
+                self.text = ''
+                self.data = ''
+
+                self.ids.layout_tarf.add_widget(self.btn1)
+                self.ids.layout_tarf.add_widget(self.btn2)
+                self.ids.layout_tarf.add_widget(self.btn3)
+                self.ids.layout_tarf.add_widget(self.btn4)
+
+                self.ids.layout_tarf.add_widget(self.label_title)
+                self.ids.layout_tarf.add_widget(self.label_text)
+                self.ids.layout_tarf.add_widget(self.label_data)
+
+                self.click = 2
+
+        def user_click_close(self):
+            if self.click == 2:
                 Animation(
                     size=(300, 150), 
                     duration=0.1,
                     t='in_quad',
                 ).start(self)
+                
                 self.ids.label_titulo.pos=(3, 45)
-                self.ids.label_data.pos=(240, 45)
+                self.ids.label_data.pos=(200, 45)
                 self.ids.label_texto.pos=(3, -35)
-                self.click = 0
 
+
+                self.ids.layout_tarf.remove_widget(self.btn1)
+                self.ids.layout_tarf.remove_widget(self.btn2)
+                self.ids.layout_tarf.remove_widget(self.btn3)
+                self.ids.layout_tarf.remove_widget(self.btn4)
+                
+                self.ids.layout_tarf.remove_widget(self.label_title)
+                self.ids.layout_tarf.remove_widget(self.label_text)
+                self.ids.layout_tarf.remove_widget(self.label_data)
+
+                self.titulo = ''
+                self.text = ''
+                self.data = ''
+                
+                self.click = 1
+        
+        def salvar(self):
+            if self.label_data.error or self.label_title.error or self.label_text.error:
+                self.show_alert()
+            else:
+                if self.click == 2:
+                    Animation(
+                        size=(300, 150), 
+                        duration=0.1,
+                        t='in_quad',
+                    ).start(self)
+                    
+                    self.ids.label_titulo.pos=(3, 45)
+                    self.ids.label_data.pos=(200, 45)
+                    self.ids.label_texto.pos=(3, -35)
+
+
+                    self.ids.layout_tarf.remove_widget(self.btn1)
+                    self.ids.layout_tarf.remove_widget(self.btn2)
+                    self.ids.layout_tarf.remove_widget(self.btn3)
+                    self.ids.layout_tarf.remove_widget(self.btn4)
+                    
+                    self.ids.layout_tarf.remove_widget(self.label_title)
+                    self.ids.layout_tarf.remove_widget(self.label_text)
+                    self.ids.layout_tarf.remove_widget(self.label_data)
+
+                    self.titulo = self.label_title.text
+                    self.text = self.label_text.text
+                    self.data = self.label_data.text
+                    
+                    self.click = 1
+   
+    def excluir(self):
+        self.ids.tarf.remove_widget(self.Tarefa)
 
     def botao_nova(self):
         self.ids.tarf.add_widget(self.Tarefa(line_color=(0.2, 0.2, 0.2, 0.8), md_bg_color='#FFFFFF',))
 
+
 class Tela_Lembretes(Screen, BackgroundColorBehavior):
-    pass
+    class Lembrete(MDCard):
+        titulo = StringProperty('')
+        text = StringProperty('')
+        click = 1
+        
+        def close_alert(self, obj):
+            self.alert.dismiss()
+        def show_alert(self):
+            close_btn = MDFlatButton(text='Ok', on_release=self.close_alert)
+            self.alert = MDDialog( title="Aviso", text="Valores incorretos. Por favor insira os valores de maneira a respeitar os campos.", buttons=[close_btn])
+            self.alert.open()
+
+        def click_open(self):
+            # t = Tela_Lembretes()
+            if self.click == 1:
+                Animation(
+                    size=(300, 300), 
+                    duration=0.1,
+                    t='in_quad',
+                ).start(self)
+
+                # self.btn1 = MDRaisedButton(text='Excluir', pos_hint={'center_x':.1, 'center_y':.07}, size_hint=(.2, 0.06), on_release=lambda x:t.excluir()) 
+                self.btn2 = MDRaisedButton(text='Salvar', pos_hint={'center_x':.47, 'center_y':.07}, size_hint=(.3, 0.06), on_release=lambda x:self.salvar())  
+                self.btn3 = MDIconButton(icon='close', pos_hint={'center_x':.05, 'center_y':.96}, on_release=lambda x:self.click_close())
+
+                self.label_title = MDTextField(text=self.titulo, hint_text='Título', size_hint=(.9, None), pos_hint={'center_x': .51, 'center_y': 0.85}, multiline=True, line_color_normal='white', line_color_focus="white", text_color_normal='black', text_color_focus='black', hint_text_color_focus="black",  max_text_length=30)
+                self.label_text = MDTextField(text=self.text, hint_text='Breve descrição da Tarefa.', size_hint=(.9, None), pos_hint={'center_x': .51, 'center_y': .649}, multiline=True, line_color_normal='white', line_color_focus="white", text_color_normal='black', text_color_focus='black', hint_text_color_focus="black", max_text_length=84)
+                 
+                self.titulo = ''
+                self.text = ''
+
+                # self.ids.layout_lemb.add_widget(self.btn1)
+                self.ids.layout_lemb.add_widget(self.btn2)
+                self.ids.layout_lemb.add_widget(self.btn3)
+
+                self.ids.layout_lemb.add_widget(self.label_title)
+                self.ids.layout_lemb.add_widget(self.label_text)
+
+                self.click = 2
+
+        def click_close(self):
+            if self.click == 2:
+                Animation(
+                    size=(300, 150), 
+                    duration=0.1,
+                    t='in_quad',
+                ).start(self)
+                
+                self.ids.label_titulo.pos=(3, 45)
+                self.ids.label_texto.pos=(3, -35)
+
+
+                # self.ids.layout_lemb.remove_widget(self.btn1)
+                self.ids.layout_lemb.remove_widget(self.btn2)
+                self.ids.layout_lemb.remove_widget(self.btn3)
+                
+                self.ids.layout_lemb.remove_widget(self.label_title)
+                self.ids.layout_lemb.remove_widget(self.label_text)
+
+                self.titulo = ''
+                self.text = ''
+                
+                self.click = 1
+
+        def salvar(self):
+                if self.label_title.error or self.label_text.error:
+                    self.show_alert()
+                else:
+                    if self.click == 2:
+                        Animation(
+                            size=(300, 150), 
+                            duration=0.1,
+                            t='in_quad',
+                        ).start(self)
+                        
+                        self.ids.label_titulo.pos=(3, 45)
+                        self.ids.label_texto.pos=(3, -35)
+
+
+                        # self.ids.layout_tarf.remove_widget(self.btn1)
+                        self.ids.layout_lemb.remove_widget(self.btn2)
+                        self.ids.layout_lemb.remove_widget(self.btn3)
+                        
+                        self.ids.layout_lemb.remove_widget(self.label_title)
+                        self.ids.layout_lemb.remove_widget(self.label_text)
+
+                        self.titulo = self.label_title.text
+                        self.text = self.label_text.text
+                        
+                        self.click = 1
+
+    # def excluir(self):
+    #     self.ids.tarf.remove_widget(self.Tarefa)
+
+    def botao_nova(self):
+        self.ids.lemb.add_widget(self.Lembrete(line_color=(0.2, 0.2, 0.2, 0.8), md_bg_color='#FFFFFF',))
+
 
 class Tela_Temporizador(Screen, BackgroundColorBehavior):
     timer_string = StringProperty()
